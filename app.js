6d378982823f53b16ba60810cf24248db36f91ea
Mirel Home <mirelalex2094@gmail.com>
2020-01-18 18:55:38 +0200

    handling the CRUD operations and URL params

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 // app.get('/', (req, res) => {
 //   res
 //     .status(200)
@@ -24,6 +26,82 @@ app.get('/api/v1/courses', (req, res) => {
     data: {
       courses
     }
+  });
+});
+
+app.get('/api/v1/courses/:id', (req, res) => {
+  console.log(req.params);
+
+  const id = req.params.id * 1;
+  const course = courses.find(el => el.id === id);
+
+  // if (id > courses.length) {
+  if (!course) {
+    return res.status(404).json({
+      status: 'failed',
+      message: 'Invalid ID'
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      course
+    }
+  });
+});
+
+app.post('/api/v1/courses', (req, res) => {
+  // console.log(req.body);
+
+  const newId = courses[courses.length - 1].id + 1;
+  const newCourse = Object.assign({ id: newId }, req.body);
+
+  courses.push(newCourse);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/courses-simple.json`,
+    JSON.stringify(courses),
+    err => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          course: newCourse
+        }
+      });
+    }
+  );
+
+  // res.send('Done');
+});
+
+app.patch('/api/v1/courses/:id', (req, res) => {
+  if (req.params.id * 1 > courses.length) {
+    return res.status(404).json({
+      status: 'failed',
+      message: 'Invalid ID'
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      course: '<Updated course here...>'
+    }
+  });
+});
+
+app.delete('/api/v1/courses/:id', (req, res) => {
+  if (req.params.id * 1 > courses.length) {
+    return res.status(404).json({
+      status: 'failed',
+      message: 'Invalid ID'
+    });
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: null
   });
 });
 
