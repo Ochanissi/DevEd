4ac45e8c745a1b9fe69ae95289c1fac375851e7c
Mirel Home <mirelalex2094@gmail.com>
2020-02-01 16:05:33 +0200

    handling the errors

--- app.js
+++ app.js
@@ -14,10 +14,10 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
-app.use((req, res, next) => {
-  console.log('Hello from the middleware!');
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log('Hello from the middleware!');
+//   next();
+// });
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -34,6 +34,15 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/courses', courseRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`
+  });
+});
+
+app.use((err, req, res, next) => {});
 
 // START SERVER
 
