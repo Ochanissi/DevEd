7b7d7d9666c50b7365b9a0050472f4b96910133b
Mirel Home <mirelalex2094@gmail.com>
2020-02-02 12:56:53 +0200

    hnadling unhandled and uncaught rejections and modelling nad creating new users

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', err => {
+  console.log('UNCAUGHT REJECTION! Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
@@ -22,6 +28,15 @@ mongoose
 // console.log(process.env);
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+process.on('undandledRejection', err => {
+  console.log('UNHANDLED REJECTION! Shutting down...');
+  console.log(err.name, err.message);
+  console.log(err);
+  server.close(() => {
+    process.exit(1);
+  });
 });
