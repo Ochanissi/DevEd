6142c787d2de231e460439ff779a964bbd246e3d
Mirel Home <mirelalex2094@gmail.com>
2020-03-01 21:29:18 +0200

    updating user data and password with api

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,4 +1,5 @@
 const Course = require('../models/courseModel');
+const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 
@@ -54,6 +55,21 @@ exports.getAccount = (req, res) => {
   });
 };
 
-exports.updateUserData = (req, res, next) => {
-  console.log('UPDATING USER ' + req.body);
-};
+exports.updateUserData = catchAsync(async (req, res, next) => {
+  // console.log('UPDATING USER ' + req.body);
+  const updatedUser = await User.findByIdAndUpdate(
+    req.user.id,
+    {
+      name: req.body.name,
+      email: req.body.email
+    },
+    {
+      new: true,
+      runValidators: true
+    }
+  );
+  res.status(200).render('account', {
+    title: 'Your account',
+    user: updatedUser
+  });
+});
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -20,6 +20,10 @@ router.get(
 router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
 router.get('/me', authController.protect, viewsController.getAccount);
 
-router.post('/submit-user-data', viewsController.updateUserData);
+router.post(
+  '/submit-user-data',
+  authController.protect,
+  viewsController.updateUserData
+);
 
 module.exports = router;
