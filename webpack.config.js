75a0eed0348251c5426ef5aa0f732ca158f9d336
Mirel Home <mirelalex2094@gmail.com>
2020-03-16 09:55:19 +0200

    building the create course page

--- webpack.config.js
+++ webpack.config.js
@@ -17,8 +17,8 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      filename: 'account.html',
-      template: './public/account.html'
+      filename: 'createcourse.html',
+      template: './public/createcourse.html'
     }),
     new MiniCssExtractPlugin({
       filename: 'bundle.css'
